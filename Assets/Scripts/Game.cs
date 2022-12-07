fadcfb0e8d1dba4cea6bf83901b228c7813378f2
Denis <d.afanasyev@xlab-game.com>
2022-12-07 17:44:48 +0300

    Мотыжник

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+ /*    [SerializeFiled]
+    private GameObject Stone;
+    [SerializeFiled]
+    private GameObject Pos;
+
+    private float m_timer = 0.0f;
+
+    void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Destroy(Instantiate(Stone, Pos.transform.position, Quaternion.identity), 20f);
+        }
+    } */
+}
